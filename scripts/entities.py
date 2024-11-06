4e526651cf99d95c016a4032872b8383bed8b7f7
TrainTo2046 <lakhemarujen@gmail.com>
2024-11-06 18:53:58 -0500

    Animation
    - added animation class
    - added player class
    - added player animation
    - jump, flip, run animation added

--- scripts/entities.py
+++ scripts/entities.py
@@ -14,10 +14,27 @@ class PhysicsEntity:
         
         self.collision = {'up': False, 'down': False, 'right': False, 'left': False}
 
+        # For Animation
+        self.action = ''
+        """
+        in animations, each frame image will have varying dimension so you add padding to the
+        edges so they have space to animate the changing sizes of images in animation
+        """
+        self.anim_offset = (-3, -3)
+        # player should be able to look right and left
+        self.flip = False
+        # set current animation to idle
+        self.set_action('idle')
+
     # collision_rect
     def rect(self):
         # (x, y) (w, h)
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+            self.animation = self.game.assets[self.type + '/' + self.action].copy()
 
     def update(self, tilemap, movement= (0, 0)):
         # resets the collision map
@@ -73,6 +90,13 @@ class PhysicsEntity:
                 # have to go and upadate players pos
                 self.pos[1] = entity_rect.y
         
+        # if you are moving right, player is facing right
+        if movement[0] > 0:
+            self.flip = False
+        # if you are moving left, player is facing left
+        if movement[0] < 0:
+            self.flip = True
+
         # apply gravity to make the player fall
         # apply acceleration by modifying velocity
         # remeber the top left is (0, 0)
@@ -81,6 +105,41 @@ class PhysicsEntity:
         if self.collision['down'] or self.collision['up']:
             self.velocity[1] = 0
 
+        # update the animation image
+        self.animation.update()
 
     def render(self, surf, offset= (0, 0)):
-        surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
+        """
+        get current frame of the animation
+        self.flip   is the x-axis flip
+        False       is the y-axis flip
+            -> don't want to flip the player upside down
+        """
+        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), 
+                  (self.pos[0] - offset[0] + self.anim_offset[0],  # x- axis
+                  self.pos[1] - offset[1] + self.anim_offset[1]))  # y- axis
+        #surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
+
+    
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'player', pos, size)
+        # check how long we have been in the air
+        self.air_time = 0
+
+    def update(self, tilemap, movement=(0, 0)):
+        super().update(tilemap, movement=movement)
+
+        # different entity will have different animation logic
+        # this air time animation logic is only for the player
+        self.air_time += 1
+
+        if self.collision['down']:
+            self.air_time = 0
+
+        if self.air_time > 4:
+            self.set_action('jump')
+        elif movement[0] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
--- scripts/utils.py
+++ scripts/utils.py
@@ -25,3 +25,37 @@ def load_images(path):
         images.append(load_image(path + '/' + img_name))
 
     return images
+
+class Animation:
+    def __init__(self, images, img_dur= 5, loop=True):
+        self.images = images
+        # animation to loop
+        self.loop = loop
+        self.img_duration = img_dur
+        # set to true if not looping and reach end
+        self.done = False
+        # keep track of where we are on our animation
+        self.frame = 0
+
+    # we create animation for each animation in the file
+    # have it somewhere it is easy to reach
+    # anytime something wants to use that animation, copy its own instance of the animation
+
+    # in python if you assign an object to the list, its a reference to the object instead of a copy
+    def copy(self):
+        # instead of returning a copy, it returns a reference
+        return Animation(self.images, self.img_duration, self.loop)
+    
+    def update(self):
+        if self.loop:
+            # % will force the animation to loop around
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+        else:
+            # when no loop, stops at the last image of the animation
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
+                self.done = True
+
+    def img(self):
+        # dividing the frame by how long the image is supposed to show for
+        return self.images[int(self.frame / self.img_duration)]
